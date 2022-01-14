d1ce2088f9f47feb87bf9a6fd9da5ef524a179fc
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2022-01-15 01:24:34 +0530

    freed up space

--- index.js
+++ index.js
@@ -5,14 +5,9 @@ var html = fs.readFileSync(__dirname+"/template.html", "utf8");
 const sample = require("./sample.json");
 var options = {
     format: "A4",
-    border: "5px",
+    border: "14px",
     orientation: "portrait",
-    header: {
-        height: "15px"
-    },
-    footer: {
-        height: "15px"
-    }
+    
 };
 
   var document = {
